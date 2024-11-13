7fb9d616a416e85533c12a3aec8a28d84d678e59
kibetwayne <kibetwayne24@gmail.com>
2024-11-13 16:55:17 +0300

    my_modules

--- main/my_modules.py
+++ main/my_modules.py
@@ -9,8 +9,7 @@ test = 'Test string'
 def find_index(to_search, target):
     #find the index of a value in a sequence
     for i, value in enumerate(to_search):
-        if value == target:
-            return i
-        return -1
+        return i if value == target else -1
+
 #!================================================================
 
