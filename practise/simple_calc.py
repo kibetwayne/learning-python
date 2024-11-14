af3b14596e8a8cad975069b258e3d12429d69026
kibetwayne <kibetwayne24@gmail.com>
2024-11-14 18:05:43 +0300

    accepts floating points

--- practise/simple_calc.py
+++ practise/simple_calc.py
@@ -3,25 +3,29 @@
 num1 = input('enter the first number: ')
 num2 = input('enter the second number: ')
 operation = input('enter the operation sign: ')
-result = 0
 
 def calculations(num1, num2, operation):
     if operation == '*':
-        result = num1 * num2
+        return num1 * num2
     elif operation == '/':
-        result = num1 / num2
+        return num1 / num2
     elif operation == '+':
-        result = num1 + num2
+        return num1 + num2
     elif operation == '-':
-        result = num1 - num2
+        return num1 - num2
     elif operation in ['^', '**']: #same as operation == ** or operation == ^
-        result = num1 ** num2
+        return num1 ** num2
     else:
-       print ('Invalid operation')
+        print ('Invalid operation')
+
+try:
+    num1 = float(num1)
+    num2 = float(num2)
+    result = calculations(num1, num2, operation)
     
-if num1.isnumeric() and num2.isnumeric:
-    calculations(num1, num2, operation)
-    print('Result:', result)
-else:
-    print( 'please enter a number')
-    
+    if result != 'Invalid operation':
+        print('Result:', result)
+    else:
+        print(result)
+except ValueError:
+    print('Please enter valid numbers.')
