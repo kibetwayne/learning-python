1ad58bb136d5475f890a017bad2d254a4e164abd
kibetwayne <kibetwayne24@gmail.com>
2025-09-27 17:28:27 +0300

    turtle graphics

---
+++ 2 indermediate/3 turtle graphics/main.py
@@ -0,0 +1,26 @@
+from turtle import Turtle, Screen
+#using an alias incase of modules with large names
+#import turtle as t
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("coral")
+
+#draw a square
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.right(90)
+
+# #draw a dashed line
+# for _ in range(10):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()    
+
+#drawing shapes
+
+
+
+screen = Screen()
+screen.exitonclick()
