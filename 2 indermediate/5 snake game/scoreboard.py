d634e6ffd688a935cb8bc9f0f1ed300d1763876a
kibetwayne <kibetwayne24@gmail.com>
2025-10-16 18:31:11 +0300

    add high score functionality using file handling

--- 2 indermediate/5 snake game/scoreboard.py
+++ 2 indermediate/5 snake game/scoreboard.py
@@ -6,7 +6,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -24,6 +25,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt", mode='w') as data:
+                data.write(f"{self.score}")
         self.score = 0
         self.update_scoreboard()
         
