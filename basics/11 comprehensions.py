a06d8a3a0cf453e7958366344d4bdb251842cb7a
kibetwayne <kibetwayne24@gmail.com>
2025-10-21 20:11:40 +0300

    list comprehension

--- basics/11 comprehensions.py
+++ basics/11 comprehensions.py
@@ -4,10 +4,10 @@
 #syntax => [expression for item in iterable if condition ]
 
 nums = [1,2,3,4,5,6,7,8,9,10]
-# my_list1 = []
-# for n in nums:   #old syntax => code isn't clean
-#     my_list1.append(n)
-# print(f'my_list1: {my_list1}')
+my_list1 = []
+for n in nums:   #old syntax => code isn't clean
+    my_list1.append(n)
+print(f'my_list1: {my_list1}')
 
 my_list2 = [n for n in nums] #better version
 print(f'my_list2: {my_list2}')
