6d9424a3315344b74a0fbafea7de3f8a6223367b
kibetwayne <kibetwayne24@gmail.com>
2025-10-23 13:57:05 +0300

    args & kwargs example

--- 2 indermediate/10 tkinter, GUI/playground.py
+++ 2 indermediate/10 tkinter, GUI/playground.py
@@ -6,3 +6,13 @@ def add(*number):
 
 result = add(1, 2, 3, 4, 5)
 print(result)  # Output: 15
+
+def calculate(n, **kwargs):
+    print(kwargs)  # prints the dictionary of the kwargs {'add': 3, 'multiply': 5}
+    
+    n += kwargs['add'] # adds 3 to n
+    n *= kwargs['multiply'] # multiplies n by 5
+    print(n) # prints 25
+    
+    
+calculate(2, add=3, multiply=5)  
